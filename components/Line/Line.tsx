5f78c5a46040f4be6bad1c91c63180fd6fba6b22
MehdiRezaii <99mehdirezaii99@gmail.com>
2022-09-20 01:07:45 +0430

    Added scroll end condition

--- components/Line/Line.tsx
+++ components/Line/Line.tsx
@@ -3,35 +3,44 @@ import styles from './Line.module.css'
 import { gsap } from 'gsap/dist/gsap'
 
 
-const Line: any = () => {
+const Line: FunctionComponent = () => {
     const [count, setCount] = useState(0);
     const [lastScroll, setLastScroll] = useState(0)
-    const lineSvgElement: any = useRef(0)
+    const lineElement = useRef<HTMLHeadingElement>(null)
 
     const scrollEvent = () => {
-        gsap.to("#Line", { height: '216px' })
-        let st = window.pageYOffset || document.documentElement.scrollTop;
-        if (st > lastScroll) {
+
+        const allHeightPage = window.innerHeight + window.scrollY
+        const st = window.pageYOffset || document.documentElement.scrollTop;
+        if (allHeightPage === document.body.offsetHeight) {
+            gsap.to(lineElement.current, { height: '100%' })
+        }
+
+        else if (st > lastScroll) {
+            gsap.to(lineElement.current, { height: '216px' })
             setCount(-st)
         }
+
         else if (st <= 50) {
             setCount(0)
-            gsap.to("#Line", { height: '0px' })
+            gsap.to(lineElement.current, { height: '0px' })
         }
+
         else {
             setCount(prev => prev - 1)
         }
+
         setLastScroll(window.scrollY)
     }
     useEffect(() => {
         document.addEventListener("scroll", scrollEvent);
-
-
-        // Adding event listeners for some element in DOM
+        return () => {
+            window.removeEventListener("scroll", scrollEvent);
+        };
     }, [count])
     return (
-        <div className={styles.Line} id="Line">
-            <div className={styles.Line__svg} ref={lineSvgElement} style={{ transform: `matrix(1, 0, 0, 1, 0, ${count})` }} >
+        <div className={styles.Line} ref={lineElement}>
+            <div className={styles.Line__svg} style={{ transform: `matrix(1, 0, 0, 1, 0, ${count})` }} >
                 <div>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1064 4569">
                         <path d="M249,0,979,862,0,1556l1038,793L21,3202l1043,608L249,4518">
