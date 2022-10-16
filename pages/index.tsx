92ca4cd95f53dd6c264e59d1b1dd7ee6eb8571cd
Mehdi Rezaii <99mehdirezaii99@gmail.com>
2022-10-16 15:54:56 +0330

    add getStaticProps to  index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -218,4 +218,12 @@ const Home: NextPage = () => {
   )
 }
 
+
+
+export async function getStaticProps() {
+  return {
+    props: {}, // will be passed to the page component as props
+  }
+}
+
 export default Home
