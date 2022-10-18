4194348ad24c576629d1e247cab0ab6f6e36cc79
Mehdi Rezaii <99mehdirezaii99@gmail.com>
2022-10-18 13:20:42 +0330

    The animation mode of the menu button is created

--- components/navBar/NavBar.tsx
+++ components/navBar/NavBar.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import vercel from '../../public/favicon.ico'
 import { useEffect, FunctionComponent, useRef } from "react"
 import { gsap } from 'gsap/dist/gsap'
+// import CSSRulePlugin from "gsap/CSSRulePlugin"
 import Link from "next/link"
 import styles from './navBar.module.css'
 import { listItemNav } from './listItemNav'
@@ -10,6 +11,7 @@ import { FiMenu } from "react-icons/fi";
 const NavBar: FunctionComponent<{}> = () => {
     const navElement = useRef<HTMLHeadingElement>(null)
     const navSx = useRef<HTMLHeadingElement>(null)
+    const refSpanBtnSx = useRef<any>(null)
     const scrollEventNavBar = () => {
         let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
         scrollTop > 10 ? gsap.to(navElement.current, { opacity: '1' }) : gsap.to(navElement.current, { opacity: '0' })
@@ -23,6 +25,12 @@ const NavBar: FunctionComponent<{}> = () => {
     // 
     const clickMenu = () => {
         let right = navSx.current?.style.right
+        let classListSpan = [...refSpanBtnSx.current?.classList]
+        if (classListSpan.includes('activeSpan')) {
+            refSpanBtnSx.current?.classList.remove('activeSpan')
+        } else {
+            refSpanBtnSx.current?.classList.add('activeSpan')
+        }
         console.log('click')
         console.log(navSx.current?.style.right)
         right !== "0px" ? gsap.to(navSx.current, { right: 0, duration: 0.5 }) : gsap.to(navSx.current, { right: -300, duration: 0.5 })
@@ -45,17 +53,18 @@ const NavBar: FunctionComponent<{}> = () => {
                             )
                         })}
                     </div>
-                    {/* sx item nav */}
-                    <div onClick={clickMenu} className={` ${styles.itemNav} items-center pt-3 sx:flex md:hidden`}>
-                        <button className="btn">
-                            <FiMenu size={30} />
+                    {/* sx btn nav */}
+                    <div onClick={clickMenu} className={`${styles.itemNav} relative items-center pt-3 sx:flex md:hidden`}>
+                        <button className="btn" >
+                            {/* <FiMenu size={30} /> */}
+                            <span ref={refSpanBtnSx} className={`${styles.spanBtnNav} spanBtnNav`}></span>
                         </button>
-
                     </div>
 
                 </div>
 
             </div>
+            {/* navBar sx */}
             <div ref={navSx} className="sx:fixed md:hidden top-14 pt-5 w-2/4 text-right z-50 bg-white shadow-lg h-full -right-80 ">
                 <ul>
                     {listItemNav.map((item: any, index: any) => {
