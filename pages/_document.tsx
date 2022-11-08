941c692bef9ab12016e59ef3503b62a1fb4826a2
MehdiRezaii <99mehdirezaii99@gmail.com>
2022-11-09 01:37:54 +0330

    Increase performance

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,16 @@ import { Suspense } from 'react'
 import Loading from '../components/Loading/Loading'
 export default function Document() {
     return (
-        <Html >
-            <Head />
-            <body className='' >
+        <Html className='font-BebasNeue'>
+            <Head >
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet" />
+
+            </Head>
+            <body className='font-BebasNeue' >
                 <Main />
-                    <NextScript />
+                <NextScript />
             </body>
         </Html>
     )
